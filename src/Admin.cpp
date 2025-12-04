200ad655d5384ba528dcde54d097fdc0b9913f4d
unknown <modrsh2002@gmail.com>
2025-12-04 16:24:24 +0200

    Implement Admin class
    Added constructor,, and displayInfo method

--- src/Admin.cpp
+++ src/Admin.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 
 Admin::Admin(const std::string& name, int id, const std::string& department)
-    : Person(name, id), department(department) {
+    : Person(name, id)/*, department(department)*/ {
     // TODO: [Mostafa Ahmed] needs to implement logic here.
+    this->department = department;
 }
 
 Admin::~Admin() {
@@ -12,4 +13,8 @@ Admin::~Admin() {
 
 void Admin::displayInfo() const {
     // TODO: [Mostafa Ahmed] needs to implement logic here.
+    std::cout << "=== Admin Information ===\n";
+    std::cout << "Name: " << name << "\n";
+    std::cout << "ID: " << id << "\n";
+    std::cout << "Department: " << department << "\n";
 }
