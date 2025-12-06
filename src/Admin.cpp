5b97e50c61d72987ec751508700a90a7ced09c05
unknown <modrsh2002@gmail.com>
2025-12-06 10:51:58 +0200

    Update admin

--- src/Admin.cpp
+++ src/Admin.cpp
@@ -1,10 +1,7 @@
 #include "SakanX/Admin.h"
 #include <iostream>
 
-Admin::Admin(const std::string& name, int id, const std::string& department)
-    : Person(name, id)/*, department(department)*/ {
-    // TODO: [Mostafa Ahmed] needs to implement logic here.
-    this->department = department;
+
 using namespace std;
 
 Admin::Admin(const string& name, int id, const string& department)
