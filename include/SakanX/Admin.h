e05ccbdf0c0a50b74eec6c2d0b5370abe93cb29a
unknown <modrsh2002@gmail.com>
2025-12-06 19:48:18 +0200

    Update Admin Files
    Add some methods to admin file

--- include/SakanX/Admin.h
+++ include/SakanX/Admin.h
@@ -2,17 +2,43 @@
 #define ADMIN_H
 
 #include "Person.h"
+#include "Student.h"
+#include "Apartment.h"
 #include <string>
+#include <vector>
+
+using namespace std;
 
 class Admin : public virtual Person {
 public:
-    Admin(const std::string& name, int id, const std::string& department);
+    Admin(const string& name, int id, const string& ssn, int age, 
+      const string& department);
+
+      Admin(const string& name, int id, const string& ssn, int age, 
+      NationalityType nationality, const string& department,
+      const vector<pair<string, string>>& contacts = {});
     ~Admin();
 
-    void displayInfo() const override;
+
+    // Getters
+    string getDepartment() const;
+    int getNumOfStudentsManaged() const;
+    
+    // Setters
+    void setDepartment(const std::string& newDepartment);
+
+
+
+    void displayInfo() const ;
+    void assignStudentToApartment(Student* student, Apartment* apartment);
+    void removeStudentFromApartment(Student* student, Apartment* apartment);
+    void viewAllApartments(const vector<Apartment*>& apartments) const;
+    void transferStudent(Student* student, Apartment* from, Apartment* to);
+    void calculateTotalRevenue(const vector<Apartment*>& apartments) const;
 
 private:
-    std::string department;
+    string department;
+    int studentsManaged;  // num of students under my management
 };
 
 #endif // ADMIN_H
