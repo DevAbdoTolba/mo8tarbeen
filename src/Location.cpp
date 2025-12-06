f8376195f9d973b6ed9110bc66ec179e77bfba12
AhmedSamirKhalaf <ahmedsamirkhalaf671@gmail.com>
2025-12-06 10:48:47 +0200

    the Location and the .gitignore files

---
+++ src/Location.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//#include "Location.h"
+using namespace std;
+
+class Location{
+    private:
+        string building;
+        int floor;
+        int roomNumber;
+    public:
+        Location(string buidling , int floor , int roomNumber){
+            this->roomNumber = roomNumber;
+            this->floor = floor;
+            this->building = buidling;
+        }
+
+        void diplayLocation(){
+            cout<< building
+                << " , Floor : "<<floor
+                <<" , RoomNumber : "<<roomNumber
+                <<endl;
+        }
+};
