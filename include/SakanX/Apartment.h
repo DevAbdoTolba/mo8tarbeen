7fffe1ad08a0371192e5abb9933d90311a155b95
Abdo Tolba <DevAbdoTolba@gmail.com>
2025-12-06 16:43:51 +0200

    Refactor Apartment class: add removeStudent method, totalHarmonyScore calculation, and enhance room management

--- include/SakanX/Apartment.h
+++ include/SakanX/Apartment.h
@@ -11,11 +11,44 @@ public:
     ~Apartment();
 
     void addStudent(Student* student);
+    Student removeStudent(Student* student);
+    int totalHarmonyScore(); // summtion the diffrances of tolrance result for each student
+
     void displayApartmentInfo() const;
 
+
+
 private:
+
+    int id;
+    enum class MetersTypes{
+        newMeter,
+        oldMeter
+    };
+
+    int apartmentArea;
+
+    bool goodBotgaz;
+    bool hasWifi;
+    bool hadRatsBefore;
+
     Location location;
     std::vector<Student*> students;
+    std::vector<std::pair<std::string, int>> rooms; // vector of pair of < room type, number of these rooms> 
+
+    int capacity; // how many can be living in this apartment
+
 };
 
+
+/*
+
+Future work :D
+
+
+
+*/
+
 #endif // APARTMENT_H
+
+
