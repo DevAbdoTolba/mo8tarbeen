d88365049b9111e17c1ca4322742bbe4820754b6
AhmeedFatehy <ahmed.heshmat@ejust.edu.eg>
2025-12-06 21:12:16 +0200

    Finalize student & SmartMatchAI implementation

--- include/SakanX/Student.h
+++ include/SakanX/Student.h
@@ -2,17 +2,36 @@
 #define STUDENT_H
 
 #include "Person.h"
+#include <Tag.h>
 #include <string>
-
+#include <vector>
+#include <set>
+using namespace std;
 class Student : public virtual Person {
 public:
-    Student(const std::string& name, int id, const std::string& major);
+    static vector<Tag*> availableTags; // Static member for all possible tags
+    Student(const string& name, int id, int age, NationalityType nationality, string ssn="", 
+                vector<pair<string, string>> contacts, char gender, const string& major, double tolerance);
     ~Student();
 
     void displayInfo() const override;
+    int calculateTotalTagValue() const;
+    void addTag(Tag* tag);
+    void removeTag(Tag* tag);
+    bool findTag(Tag* tag);
+    void clearTags();
+    const set<Tag*>& Student::getTags() const;
+    
+    string getMajor() const;
+    void setMajor(const string& major);
+    double getTolerance();
+    void setTolerance(double level);
 
+    void cleanupTags();
 private:
-    std::string major;
+    string major;
+    double toleranceLevel;
+    set<Tag*> tags;
 };
 
 #endif // STUDENT_H
